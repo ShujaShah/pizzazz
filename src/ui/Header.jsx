397a89556831f62d163afbe82f87d45d1d7f11d6
shuja.ashraf11@gmail.com <shuja.shah@iquasar.com>
2024-04-06 17:12:13 +0530

    implemented search query

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 const Header = () => {
   return (
     <header>
       <Link to="/">Pizzazz</Link>
+      <SearchOrder />
       <p>
         <span>23 Pizzas</span>
         <span>$23.45</span>
