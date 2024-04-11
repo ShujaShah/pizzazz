6141166795089762c9baef6b8d7c43e8afe219a9
shujashah <shuja.ashraf11@gmail.com>
2024-04-11 23:02:35 +0530

    implemented the delete pizza from cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -49,6 +49,9 @@ export const getTotalCartQuantity = (state) =>
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+
 export const {
   addItem,
   deleteItem,
